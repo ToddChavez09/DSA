33d98d67ae0483eefdc9136ca9b681d4e4bc3fcd
nikhilvashisht <nikhilvashisht50@gmail.com>
2022-10-16 13:51:14 +0530

    Added search algorithm in BST

---
+++ BST/SearchForKeyInBST.java
@@ -0,0 +1,42 @@
+package BST;
+
+class searchForKeyInBST {
+
+    public class Node {
+        int val;
+        Node left;
+        Node right;
+    
+        Node(){}
+        
+        Node(int val){
+            this.val = val;
+        }
+    
+        Node (int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public boolean searchBST (int key, Node root) {
+        if(root == null) {
+            return false;
+        }
+    
+        if (root.val == key) {
+            return true;
+        }
+    
+        //search left tree
+        boolean leftSearch = searchBST(key, root.left);
+    
+        //search right tree
+        boolean rightSearch = searchBST(key, root.right);
+    
+        return leftSearch || rightSearch;
+    }
+    
+
+}    
